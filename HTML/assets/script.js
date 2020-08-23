9fe59bff4146f90b1e8f39a7210746be5323a382
Wojciech Langowski <wojciech.langowski@gmail.com>
2020-08-23 08:58:45 +0200

    Koniec zajęć w sobotę 2020-08-22

--- HTML/assets/script.js
+++ HTML/assets/script.js
@@ -1,1 +1,14 @@
 console.log('Hello world!');
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("navbar").style.padding = "20px 10px";
+    } else {
+        document.getElementById("navbar").style.padding = "40px 10px";
+    }
+}
+// hoisting
